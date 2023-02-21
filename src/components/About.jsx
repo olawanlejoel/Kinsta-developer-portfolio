045077640364b073d09b5a5fd090a91362b9b984
Maciek Palmowski <m.palmowski@freshpixels.pl>
2023-02-21 23:30:37 +0100

    Fixed images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
                     <p> error ratione asperiores dignissimos libero sed quis harum nesciunt culpa dolores! Est dolore nobis ipsum!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut atque autem dolor nisi voluptas iste, error ratione asperiores dignissimos libero sed quis harum nesciunt culpa dolores! Est dolore nobis ipsum!</p>
                 </div>
                 <div className="about-img">
-                    <Image src='/about.jpeg' className="profile-img" width={300} height={500} />
+                    <Image src='/images/about.jpeg' className="profile-img" width={300} height={500} />
                 </div>
             </div>
         </div>
